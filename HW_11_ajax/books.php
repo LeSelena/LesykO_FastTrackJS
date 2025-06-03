bbd2c437551cdba19d42d82b48334a4b93c6cc8b
LeSelena <129741183+LeSelena@users.noreply.github.com>
2025-06-03 10:46:12 +0200

    added HW_11
    added HW_11

---
+++ HW_11_ajax/books.php
@@ -0,0 +1,8 @@
+<?php
+echo "<ul>
+  <li>David Flanagan</li>
+  <li>Kyle Simpson</li>
+  <li>John Resig</li>
+  <li>Jeremy Keith</li>
+</ul>";
+?>
---
+++ HW_11_ajax/vote.php
@@ -0,0 +1,4 @@
+<?php
+date_default_timezone_set("UTC");
+echo "Your vote is accepted " . date("d M Y H:i:s");
+?>
